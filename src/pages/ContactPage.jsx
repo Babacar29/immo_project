e36fddc94fe3975c0a69dc2e8ddde9357917bdf5
Babacar Diouf <babacardiouf29@gmail.com>
2025-10-04 11:32:40 +0000

    fix: debug contact page ui

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -76,7 +76,7 @@ const ContactPage = () => {
     <div className="min-h-screen bg-secondary">
       <Navbar />
       
-      <div className="py-12">
+      <div className="pt-28 pb-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           
           <motion.div
